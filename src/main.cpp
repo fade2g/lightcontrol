37c47a1e618e2b3410feb586f9e5304d1ff83ef8
fade2g <holger.heymanns@gmail.com>
2018-01-02 18:44:58 +0100

    Implement some basic potentiometer reading
    Controls the speed of the LED flashing

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,38 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
+#include "pins.cpp";
 
-const int LED_PIN = 0;
+const int LED_PIN = DEFAULT_LED_PIN;
+int potValue;
+int iterations;
+int iteration = 0;
+int state = LOW;
+
 
 void setup() {
+    Serial.begin(115200);
     pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH);
-    delay(200);
-    digitalWrite(LED_PIN, LOW);
-    delay(200);
+    potValue = analogRead(POT_PIN_0);
+    iterations = potValue / DELAY_INTERVAL;
+    iteration++;
+    if (iteration > iterations) {
+        if (state == LOW) {
+            state = HIGH;
+        } else {
+            state = LOW;
+        }
+        iteration = 0;
+    }
+    Serial.print("Pot: ");
+    Serial.print(potValue);
+    Serial.print("iteration");
+    Serial.print(iteration);
+    Serial.print("iterations");
+    Serial.println(iterations);
+    digitalWrite(LED_PIN, state);
+    delay(DELAY_INTERVAL);
 }
---
+++ src/pins.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Holger on 02.01.2018.
+//
+#include <Arduino.h>
+
+const int DEFAULT_LED_PIN = 13;
+const int DELAY_WAIT = 1000;
+const int DELAY_INTERVAL = 10;
+const int POT_PIN_0 = A0;
