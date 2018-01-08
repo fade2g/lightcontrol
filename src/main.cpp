7f08ddb66d2540f76194bc519ef85cb3bc24fe3b
fade2g <holger.heymanns@gmail.com>
2018-01-08 22:24:30 +0100

    Implement some basic code with a lot of comments
    This is actually only playing around and trying different things
    Now cleanup has been done so far.

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,148 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
-#include "pins.cpp";
+// #include "pins.cpp"
 
-int potValue;
-int previousPotValue;
-int hueValue;
+const int LED_PIN = 12;
+const int DELAY_WAIT = 20;
+const int DELAY_INTERVAL = 20;
+const int NUM_LEDS = 12;
+
+const int POT_PIN_0 = A0;
+const int POT_PIN_1 = A1;
+const int POT_PIN_2 = A2;
+
+
+struct CircleAnmiationHolder {
+    int speed;
+    int frame;
+};
+
+int potValue0;
+int potValue1;
+int potValue2;
+int previousPotValue0;
+int previousPotValue1;
+int previousPotValue2;
+byte hueValue = 0;
+byte saturationValue = 0;
+byte brightnessValue = 100;
+boolean changed;
 
 CRGB leds[NUM_LEDS];
 
-void setup() {
-    Serial.begin(115200);
-    pinMode(LED_PIN, OUTPUT);
-    LEDS.addLeds<WS2812, LED_PIN,GRB>(leds,NUM_LEDS);
-    LEDS.setBrightness(84);
+byte normalizeValue(int value) {
+    return map(value, 0, 1023, 0, 255);
 }
 
-void loop() {
-    potValue = analogRead(POT_PIN_0);
-    if (previousPotValue != potValue) {
-        hueValue = potValue / 4;
-        for(int i = 0; i < NUM_LEDS; i++) {
-            // Set the i'th led to red
-            leds[i] = CHSV(hueValue, 255, 255);
-            // Show the leds
-            FastLED.show();
-        }
-        previousPotValue = potValue;
+void handlePotChange0(int value) {
+    previousPotValue0 = value;
+    hueValue = normalizeValue(value);
+}
+
+void handlePotChange1(int value) {
+    previousPotValue1 = value;
+    saturationValue = normalizeValue(value);
+}
+
+void handlePotChange2(int value) {
+    previousPotValue2 = value;
+    brightnessValue = normalizeValue(value);
+}
+
+void updateLedsHsv(int hueValue, int saturationValue, int brightnessValue) {
+    for(int i = 0; i < NUM_LEDS; i++) {
+        // Set the i'th led to red
+        leds[i] = CHSV(hueValue, saturationValue, brightnessValue);
     }
-    Serial.print("Pot: ");
-    Serial.print(potValue);
-    Serial.print("Hue: ");
-    Serial.println(hueValue);
-    delay(DELAY_INTERVAL);
+    fadeToBlackBy( leds, NUM_LEDS, 20);
+    // Show the leds
+    FastLED.show();
+}
+
+void fillColor(byte hue, byte sat, byte val) {
+    CHSV color = CHSV(hue, sat, val);
+    fill_solid(leds, NUM_LEDS, color);
+    fadeToBlackBy( leds, NUM_LEDS, 20);
+}
+
+
+void circleAnimation(CircleAnmiationHolder holder) {
+}
+
+void setup() {
+    Serial.begin(115200);
+    // pinMode(LED_PIN, OUTPUT);
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(84);
+    potValue0 = 0;
+    potValue1 = 0;
+    potValue2 = 0;
+    previousPotValue0 = 1;
+    previousPotValue1 = 1;
+    previousPotValue2 = 1;
+    updateLedsHsv(55, 200, 90);
+}
+
+int angle = 0;
+int speed = 15;
+
+void loop() {
+    potValue0 = analogRead(POT_PIN_0);
+    potValue1 = analogRead(POT_PIN_1);
+    potValue2 = analogRead(POT_PIN_2);
+
+    if (previousPotValue0 != potValue0) {
+        handlePotChange0(potValue0);
+        changed=true;
+    }
+
+    if (previousPotValue1 != potValue1) {
+        handlePotChange1(potValue1);
+        changed=true;
+    }
+
+    if (previousPotValue2 != potValue2) {
+        speed = map(potValue2, 0, 1024, 3, 30);
+        changed=true;
+    }
+
+    Serial.print("PotValue");
+    Serial.print(potValue2);
+    Serial.print(", speed: ");
+    Serial.println(speed);
+
+    EVERY_N_MILLIS_I( thistimer, speed ) { // initial period = 100ms
+        thistimer.setPeriod( speed ); // random period 10..200ms
+        uint8_t lead_dot = map(sin8(angle), 0, 255, 0, NUM_LEDS - 1);
+        angle = angle + 1;
+        leds[lead_dot] = CHSV( hueValue, saturationValue, 96);;
+
+        fadeToBlackBy(leds, NUM_LEDS, 32);
+    }
+    FastLED.show();
+
+/*
+    if (previousPotValue2 != potValue2) {
+        handlePotChange2(potValue2);
+        changed=true;
+    }
+
+    if(changed) {
+//        updateLedsHsv(hueValue, saturationValue, brightnessValue);
+//        updateLedsHsv(hueValue, saturationValue, brightnessValue);
+        changed=false;
+    }
+    fadeToBlackBy( leds, NUM_LEDS, 20);
+    Serial.print("hue: ");
+    Serial.print(hueValue);
+    Serial.print("saturation: ");
+    Serial.print(saturationValue);
+    Serial.print("brightness: ");
+    Serial.println(brightnessValue);
+    // updateLedsHsv(hueValue, saturationValue, brightnessValue);
+    fillColor(200, 20, 20);
+    FastLED.show();
+*/
+//     delay(DELAY_INTERVAL);
 }
--- src/pins.cpp
+++ src/pins.cpp
@@ -3,8 +3,11 @@
 //
 #include <Arduino.h>
 
-const int LED_PIN = 13;
+const int LED_PIN = 3;
 const int DELAY_WAIT = 1000;
-const int DELAY_INTERVAL = 10;
-const int POT_PIN_0 = A0;
+const int DELAY_INTERVAL = 50;
 const int NUM_LEDS = 12;
+
+const int POT_PIN_0 = A0;
+const int POT_PIN_1 = A1;
+const int POT_PIN_2 = A2;
